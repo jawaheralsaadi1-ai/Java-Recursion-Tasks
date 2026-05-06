b695d98405956b801ebec15cf8971a943acf2255
jawaher <jawaheralsaadi!@gmail.com>
2026-05-06 16:47:29 +0400

    Add: define int factorial class and method.

--- src/tasksFrom1To21/Task1Factorial.java
+++ src/tasksFrom1To21/Task1Factorial.java
@@ -1,6 +1,12 @@
 package tasksFrom1To21;//
 
 // Task 1: Factorial Calculation
-class Task1Factorial {
+// Goal : Recursive method to calculate n!
+public class Task1Factorial {
 
+    //Create a method named factorial
+    public int factorial(int n) {
+
+        return n;
+    }
 }
