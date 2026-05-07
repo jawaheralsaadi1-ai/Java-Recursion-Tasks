bae032cc9f3af641a0146c831266b331cc7fa0d1
jawaher <jawaheralsaadi!@gmail.com>
2026-05-07 13:50:07 +0400

    Edit (Task2): Define bunnyEars2 structure and base case.

--- src/tasksFrom1To21/Task2BunnyEars.java
+++ src/tasksFrom1To21/Task2BunnyEars.java
@@ -6,10 +6,9 @@ public class Task2BunnyEars {
     public int bunnyEars(int n) {
         //Base cause :  If there are no bunnies, there are no ears.
         if (n == 0) return 0;
-        if (n % 2 == 0) {
-            return 1 + bunnyEars(n / 2);
-        }
         return n;
-        
     }
 }
+//TODO, STEP 2: IMPLEMENT CONDITIONAL RECURSION
+
+
