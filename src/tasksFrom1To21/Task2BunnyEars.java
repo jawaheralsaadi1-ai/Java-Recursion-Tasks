e99528c44178b44f09fd04947c89de27ccc8bb6a
jawaher <jawaheralsaadi!@gmail.com>
2026-05-07 14:05:10 +0400

    add (Task2):Implement Condition RECURSION :   Even/Odd logic

--- src/tasksFrom1To21/Task2BunnyEars.java
+++ src/tasksFrom1To21/Task2BunnyEars.java
@@ -6,9 +6,16 @@ public class Task2BunnyEars {
     public int bunnyEars(int n) {
         //Base cause :  If there are no bunnies, there are no ears.
         if (n == 0) return 0;
-        return n;
+        // STEP 2: IMPLEMENT CONDITIONAL RECURSION
+        // Implement Even/Odd logic
+        if (n % 2 == 0){ // Check if the current bunny index is Even
+            return 3 + bunnyEars(n - 1);
+        }
+        else { //Otherwise  the curren bunny index is Odd
+            return 2 + bunnyEars(n - 1);
+        }
     }
 }
-//TODO, STEP 2: IMPLEMENT CONDITIONAL RECURSION
+
 
 
