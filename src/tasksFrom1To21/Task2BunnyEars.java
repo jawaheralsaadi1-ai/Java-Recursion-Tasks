4bcb139fbca71c315a556ac93cf09cf987505122
jawaher <jawaheralsaadi!@gmail.com>
2026-05-07 13:44:02 +0400

    add (Task2): Define bunnyEars2 structure and base case.

--- src/tasksFrom1To21/Task2BunnyEars.java
+++ src/tasksFrom1To21/Task2BunnyEars.java
@@ -1,5 +1,15 @@
 package tasksFrom1To21; //
 
 public class Task2BunnyEars {
-//TODO:// STEP 1: INITIALIZE STRUCTURE & BASE CASE
+    // STEP 1: INITIALIZE STRUCTURE & BASE CASE
+    // Create method and define the stopping point.
+    public int bunnyEars(int n) {
+        //Base cause :  If there are no bunnies, there are no ears.
+        if (n == 0) return 0;
+        if (n % 2 == 0) {
+            return 1 + bunnyEars(n / 2);
+        }
+        return n;
+        
+    }
 }
