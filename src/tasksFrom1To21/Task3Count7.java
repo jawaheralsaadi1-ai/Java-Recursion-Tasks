315f510be104058c4b161e849178b4a7f0ddfe96
jawaher <jawaheralsaadi!@gmail.com>
2026-05-10 10:40:31 +0400

    add (Task3): Define Count7 class structure and base case.

--- src/tasksFrom1To21/Task3Count7.java
+++ src/tasksFrom1To21/Task3Count7.java
@@ -1,5 +1,14 @@
 package tasksFrom1To21;
 
 public class Task3Count7 {
-    //TODO:// STEP 1: INITIALIZE STRUCTURE & BASE CASE
+    // STEP 1: INITIALIZE STRUCTURE & BASE CASE
+    // Create method and define the stopping point.
+    public int count7(int n) {
+        //Base cause :If the number is 0, we have finished checking all digits.
+        if (n == 0) {
+            return 0;
+        }
+return n ;
+    }
+    //Next STEP 2: IMPLEMENT CONDITIONAL RECURSION
 }
