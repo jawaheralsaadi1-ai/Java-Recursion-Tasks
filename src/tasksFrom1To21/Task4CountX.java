3d920255755037ea87bafd2de9275b9f21e3b790
jawaher <jawaheralsaadi!@gmail.com>
2026-05-11 11:29:24 +0400

    Add (Task4): DEFINE BASE CASE inside countX method.

--- src/tasksFrom1To21/Task4CountX.java
+++ src/tasksFrom1To21/Task4CountX.java
@@ -2,11 +2,15 @@ package tasksFrom1To21;
 
 /**
  * Task 4: Create a Recursive CountX Program in Java
- * Goal: Counting specific tags in a document or occurrences of a
+ * Goal: Counting specific lowercase character 'x'
+ * appears in a string
  */
 
 public class Task4CountX {
     public int countX(String str) {
+        // Base Case: If string is empty, we reached the end of our search.
+        if (str == null || str.length() == 0) return 0;
+
         return 0;
     }
 }
